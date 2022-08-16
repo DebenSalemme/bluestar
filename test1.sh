91fcabc9b1970ab5ecd434764c78542fdf6a0c47
Deben <debensalemme@gmail.com>
2022-08-16 20:58:43 +0200

    Create test1.sh

---
+++ test1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello World"
+echo "Ciao sono uno script di test, ti scrivo dal repository di Github"
+echo "debug - Manda testo su un file nella workarea"
+echo "Queste sono linee di test per il file nella workarea" >> test.txt
+echo "debug - Testo mandato per il file nella workarea test.txt"
