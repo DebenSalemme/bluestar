3be2ba921dfef54f0abac13b32cddf5780abea7f
deben <debensalemme@gmail.com>
2022-08-16 16:36:18 -0400

    debug4 - modifica riga

--- test1.sh
+++ test1.sh
@@ -3,6 +3,6 @@
 echo "Hello World"
 echo "Ciao sono uno script di test, ti scrivo dal repository di Github"
 echo "debug - Manda testo su un file nella workarea"
-echo "debug3 - modifica riga"
+echo "debug4 - modifica riga"
 echo "Queste sono linee di test per il file nella workarea" >> test.txt
 echo "debug - Testo mandato per il file nella workarea test.txt"
